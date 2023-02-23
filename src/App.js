5ed2d9f13968ef153559ef079c0c0aad72032193
AdrienLD <adrien.laduca@icloud.com>
2023-02-23 23:46:29 +0100

    Finish login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Header from './Header'
+import Home from './Home';
 import Login from './Login';
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path='/checkout' element={
-          <><Header /></>
+          <Route exact path='/' element={
+          <><Header /><Home/></>
           }/>
           <Route exact path='/login' element={<Login/>}/>
         </Routes>
