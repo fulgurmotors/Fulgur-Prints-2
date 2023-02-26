de372fb85a218383fc8461bb9dbf07dc04d64234
AdrienLD <adrien.laduca@icloud.com>
2023-02-26 14:27:51 +0100

    better login

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import logo from './Images/Fulgur_prints.png'
 import './Login.css'
+import LockIcon from '@material-ui/icons/LockOutlined'
+import MailIcon from '@material-ui/icons/MailOutline'
 
 function Login(){
     return(
         <div className='login'>                
             <img className='login__logo' alt='logo' src={logo}/>
-            
             <div className='login__container'>
                 <h1>Connexion</h1>
-                <hr className='login__verticalBar'/>
-                <h2>E-mail</h2>
-                <input type='email'/>
-                <h2>Mot de Passe</h2>
-                <input type='password'/>
-                <button type='submit' className='login__signInButton'>Se connecter</button>
+                <hr className='login__horizontalBar'/>
+                <div className='login__containerBox'>
+                    <div className="login__inputBox">
+                        <MailIcon className='login__optionIcon'/>
+                        <input type='email' required/>
+                        <label>E-mail</label>
+                    </div>
+                    <div className="login__inputBox">
+                        <LockIcon className='login__optionIcon'/>
+                        <input type='password' required/>
+                        <label>Mot de Passe</label>
+                    </div>
+                    <button type='submit' className='login__signInButton'>Se connecter</button>
+                </div>
             </div>
         </div>
     )
