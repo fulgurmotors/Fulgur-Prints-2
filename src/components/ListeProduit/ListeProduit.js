f551d9a3fe9ce7227192385c2b0f4202a38afc17
AdrienLD <adrien.laduca@icloud.com>
2023-05-30 19:48:55 +0200

    modifs mineures

--- src/components/ListeProduit/ListeProduit.js
+++ src/components/ListeProduit/ListeProduit.js
@@ -2,33 +2,41 @@ import React from 'react'
 import './ListeProduit.css'
 import Produit from '../Produits/Product'
 import { Link } from 'react-router-dom'
+import { produits } from '../productsList'
 
 function ListeProduit() {
+
+    const getRandomId = () => {
+        const randomIndex = Math.floor(Math.random() * produits.length)
+        return produits[randomIndex].id
+    }
+
+
     return (
         <div className='liste'>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
             <Link to='/produit/0G481' style={{ textDecoration: 'none', color: 'black' }} >
-                <Produit id='0G481' />
+                <Produit id={getRandomId()} />
             </Link>
         </div>
 
--- src/components/productsList.js
+++ src/components/productsList.js
@@ -3,27 +3,36 @@ export const produits = [
     id: '0G481',
     nom: 'Pot de Fleur',
     prix: 25.99,
-    dimensions: [ 10, 10, 10],
-    couleur: ['red', 'blue'],
-    poids: '500g',
+    dimensions: { longueur: 20, largeur: 20, hauteur: 20 },
+    couleur: ['red', 'blue', 'green', 'yellow', 'white', 'black'],
+    poids: '1.2 kg',
     description: 'Un pot de fleur élégant et moderne qui convient à tout type de plante d\'intérieur.'
   },
   {
     id: 'YFJBZ',
-    nom: 'Support de téléphone',
+    nom: 'Support de Téléphone',
     prix: 12.99,
-    dimensions: [8, 8, 12],
-    couleur: 'Noir',
-    poids: '200g',
+    dimensions: { longueur: 10, largeur: 10, hauteur: 15 },
+    couleur: ['black', 'blue', 'red', 'green', 'yellow'],
+    poids: '0.5 kg',
     description: 'Un support de téléphone universel qui convient à tous les types de téléphones portables.'
   },
   {
-    id: 'YF3CZ',
+    id: 'T7S3D',
     nom: 'Dé',
     prix: 5,
-    dimensions: [3, 3, 3],
-    couleur: ['blue'],
-    poids: '200g',
-    description: 'Un support de téléphone universel qui convient à tous les types de téléphones portables.'
+    dimensions: { longueur: 5, largeur: 5, hauteur: 5 },
+    couleur: ['white'],
+    poids: '0.1 kg',
+    description: 'Un dé classique idéal pour les jeux de société et les jeux de hasard.'
+  },
+  {
+    id: 'DAIVK',
+    nom: 'Voiture',
+    prix: 5,
+    dimensions: { longueur: 10, largeur: 5, hauteur: 3 },
+    couleur: ['red', 'white'],
+    poids: '0.2 kg',
+    description: 'Une petite voiture miniature parfaite pour les collectionneurs et les enfants.'
   }
-]
+];
